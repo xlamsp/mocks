8b756c70fa145f9f4bfeda05b1366e0be9486ae6
xlamsp <xlamsp@gmail.com>
2020-01-18 11:04:09 -0500

    Added MockInit test group with dummy test

--- test/test_mocks.c
+++ test/test_mocks.c
@@ -1,6 +1,11 @@
 #include "unity_fixture.h"
 
+static void RunAllTests(void)
+{
+  RUN_TEST_GROUP(MocksInit);
+}
+
 int main(int argc, const char **argv)
 {
-  return 0;
+  return UnityMain(argc, argv, RunAllTests);
 }
